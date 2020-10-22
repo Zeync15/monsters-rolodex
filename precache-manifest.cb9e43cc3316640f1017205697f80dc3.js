d06e8a8f59522417ab02a53d8ea14774b6660d03
Ze Yen Chai <zeyenchai@gmail.com>
2020-10-23 02:52:32 +0800

    Updates

---
+++ precache-manifest.cb9e43cc3316640f1017205697f80dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f096b90c577759ac2cdc6ac52d766f47",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c39080da416664712eb5",
+    "url": "/monsters-rolodex/static/css/main.48178711.chunk.css"
+  },
+  {
+    "revision": "f446d9444977f330bfbe",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.70969f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c39080da416664712eb5",
+    "url": "/monsters-rolodex/static/js/main.4b4a417d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
